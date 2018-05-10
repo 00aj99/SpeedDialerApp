7f951e3f677f0918fd87f7b42a037ff25d77b2a5
denmlaa <denmlaa95@hotmail.com>
2018-05-10 12:29:36 +0200

    Welcome screen design change

--- app/src/main/java/com/example/denmlaa/speeddialerapp/activities/SplashWelcomeActivity.java
+++ app/src/main/java/com/example/denmlaa/speeddialerapp/activities/SplashWelcomeActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.denmlaa.speeddialerapp.R;
@@ -21,15 +22,18 @@ import com.example.denmlaa.speeddialerapp.R;
 public class SplashWelcomeActivity extends AppCompatActivity {
 
     private TextView splash_text;
+    private ImageView splash_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_welcome);
 
+        splash_image = findViewById(R.id.splash_image);
         splash_text = findViewById(R.id.splash_text);
         Animation anim = AnimationUtils.loadAnimation(this, R.anim.splash_animation);
         splash_text.startAnimation(anim);
+        splash_image.startAnimation(anim);
 
         // Delayed check for permissions for 2000 milisec. Check starts after animation is done
         new Handler().postDelayed(new Runnable() {
